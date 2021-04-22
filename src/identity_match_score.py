1bf90d640adc3ae46d5d589f6ebf257887e0f805
pierrz <pierrz@tuta.io>
2021-04-22 23:59:41 +0200

    calculation node ready

--- src/identity_match_score.py
+++ src/identity_match_score.py
@@ -1,11 +1,6 @@
+from typing import Union
 import pandas as pd
-from nltk.corpus.reader.wordnet import lin_similarity
-
-# from nltk.corpus import wordnet as wn
-import nltk
-
-nltk.download("wordnet")
-wn = nltk.corpus.wordnet
+from nltk.metrics.distance import edit_distance
 
 data = {
     "pair1": {
@@ -30,6 +25,15 @@ data = {
     },
 }
 
+"""
+TODO:
+- unit test
+- auth
+- doc
+- docker & tox
+- SAP
+"""
+
 
 class IdentityMatchDataframe:
     raw_data = pd.DataFrame.from_dict(data, orient="index")
@@ -38,7 +42,7 @@ class IdentityMatchDataframe:
 
     def __init__(self):
         self.clean_data = self.prepare_clean_data()
-        self.get_fullname_similarity_score()
+        self.similarity_score = self.calculate()
 
     def prepare_clean_data(self):
         df_list = list()
@@ -60,21 +64,45 @@ class IdentityMatchDataframe:
         return self.raw_data["identity_match_score"].to_frame().join(df_list)
 
     def calculate(self):
-        bsn_mask = self.clean_data["bsn1"] == self.clean_data["bsn2"]
-        different_bsn_scores = 0.0
-        results = pd.Series(1.0, index=self.clean_data.index).where(
-            bsn_mask, other=different_bsn_scores
+        """
+        - If the dates of birth are known and not the same, there is no match
+        - If the BSN number matches then 100%
+        - Otherwise:
+            o If the last name is the same: +40%
+            o If the first name is the same: +20%
+            o If the first name is similar: +15%
+            o If the date of birth matches: + 40%
+        """
+
+        identical_bsn_mask = self.compare_dates_or_integers("bsn1", "bsn2")
+        not_identical_birthdate_mask = self.compare_dates_or_integers(
+            "birthdate1", "birthdate2", "not"
+        )
+        no_match_values = pd.Series(0.0, index=self.clean_data.index)
+        match_values = pd.Series(1.0, index=self.clean_data.index)
+
+        no_bsn_date_match_values = (
+            self.get_last_name_score()
+            + self.get_first_name_score()
+            + self.get_birthdate_score()
+        )
+
+        return no_match_values.where(
+            not_identical_birthdate_mask,
+            other=match_values.where(
+                identical_bsn_mask, other=no_bsn_date_match_values
+            ),
         )
 
     def get_notna_mask(self, col1_name: str, col2_name: str):
         return self.clean_data[col1_name].notna() & self.clean_data[col2_name].notna()
 
-    def compare_dates_or_integers(self, col1_name: str, col2_name: str):
+    def compare_dates_or_integers(self, col1_name: str, col2_name: str, *args):
         not_na_mask = self.get_notna_mask(col1_name, col2_name)
-        identical_values_mask = (
-            self.clean_data[col1_name] == self.clean_data[col2_name]
-        ) & not_na_mask
-        return identical_values_mask
+        identical_values_mask = self.clean_data[col1_name] == self.clean_data[col2_name]
+        if len(args) == 1:
+            return ~identical_values_mask & not_na_mask
+        return identical_values_mask & not_na_mask
 
     def get_bsn_score(self):
         identical_bsn_mask = self.compare_dates_or_integers("bsn1", "bsn2")
@@ -95,15 +123,19 @@ class IdentityMatchDataframe:
         return values.str.replace(".* ", " ", regex=True)
 
     @staticmethod
-    def get_first_name_string(values: pd.Series) -> pd.Series:
+    def get_first_name_string(values: Union[pd.Series, str]) -> Union[pd.Series, str]:
+        if type(values) == str:
+            values = pd.Series(values)
         return values.str.replace(" .*", " ", regex=True)
 
     def get_last_name_score(self):
         col1_name, col2_name = self.fullname_cols
         not_na_mask = self.get_notna_mask(col1_name, col2_name)
+
         last_name_mask = self.get_last_name_string(
             self.clean_data[col1_name]
         ) == self.get_last_name_string((self.clean_data[col2_name]))
+
         return pd.Series(0.4, index=self.clean_data.index).where(
             (last_name_mask & not_na_mask), other=0.0
         )
@@ -111,51 +143,44 @@ class IdentityMatchDataframe:
     def get_first_name_score(self):
         col1_name, col2_name = self.fullname_cols
         not_na_mask = self.get_notna_mask(col1_name, col2_name)
+
         first_name_mask = self.get_first_name_string(
             self.clean_data[col1_name]
         ) == self.get_first_name_string((self.clean_data[col2_name]))
+
         return pd.Series(0.2, index=self.clean_data.index).where(
-            (first_name_mask & not_na_mask), other=0.0
+            (first_name_mask & not_na_mask), other=self.get_fullname_similarity_score()
         )
 
     def get_fullname_similarity_score(self):
         col1_name, col2_name = self.fullname_cols
+        default_score = 0.15
 
         score_values = list()
         for idx, r in self.clean_data[self.fullname_cols].iterrows():
-            # print(lin_similarity(r[col1_name], r[col2_name], ic="semcor_ic"))
-            score_values.append(
-                lin_similarity(
-                    wn.synset(f"{r[col1_name]}.n.01"),
-                    wn.synset(f"{r[col2_name]}.n.01"),
-                    ic="semcor_ic",
-                )
+
+            # Calculate the Levenshtein edit-distance between two strings
+            string_distance = edit_distance(
+                r[col1_name],
+                r[col2_name],
+                substitution_cost=1,
+                transpositions=False,
             )
 
-        self.clean_data["similarity_score"] = score_values
+            # check if 2nd string is either initials or similar enough for typo/diminutive
+            if (
+                r[col2_name] == f"{self.get_first_name_string(r[col2_name])[0]}."
+                or string_distance <= 6
+            ):
+                r_score = default_score
+            else:
+                r_score = 0.0
+            score_values.append(r_score)
 
-        # wn.synset(f"{r[col1_name]}.n.01")
-        # df = self.clean_data[self.fullname_cols].apply(lambda x: lin_similarity(
-        #     synset1=x[col1_name],
-        #     synset2=x[col2_name],
-        #     ic="semcor_ic"
-        # ))
-        # return df
+        return pd.Series(score_values, index=self.clean_data.index)
 
 
 if __name__ == "__main__":
     df = IdentityMatchDataframe()
-    # print(df.raw_data)
-    print(df.clean_data)
-    print(df.clean_data["similarity_score"])
-    # print(df.get_fullname_similarity_score())
-
-    """
-    - If the BSN number matches then 100%
-    - Otherwise:
-        o Ifthelastnameisthesame+40%
-        o Ifthefirstnameisthesame+20%
-    o Ifthefirstnameissimilar+15%(seeexamples)
-        o If the date of birth matches + 40%
-    o If the dates of birth are known and not the same, there is no match
-    """
+    # print(df.clean_data)
+    print(df.similarity_score)

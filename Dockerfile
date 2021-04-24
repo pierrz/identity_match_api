52c7a3d6493247b745c8f65a88096fde22d9011a
pierrz <pierrz@tuta.io>
2021-04-24 13:33:25 +0200

    dedicated branch for docker

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,6 @@ FROM tiangolo/uvicorn-gunicorn-fastapi:python3.8
 COPY ./data /data
 COPY ./app /app
 RUN pip install --upgrade pip
-RUN pip install -r requirements.txt
+RUN pip install -r requirements/default.txt
 RUN pip install -e .
 CMD uvicorn src.main:app --reload
--- app/src/main.py
+++ app/src/main.py
@@ -11,34 +11,33 @@ from fastapi import Depends, FastAPI, HTTPException, status, Request
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from pydantic import BaseModel
 
+
 app = FastAPI()
-# default_data_path = Path(os.getcwd(), "data", "examples.json")
 default_data_path = Path(os.sep, "data", "examples.json")
 templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
 security = HTTPBasic()
 
+
 class ScoresFrameAPI(BaseModel):
 
-    data: any
+    source_path: Optional[str] = None
+    data: pd.Series
 
     def prepare_results(self, file_path: str = default_data_path):
         processed_data = IdentityMatchDataframe()
         processed_data.import_and_process_data(file_path)
         self.data = processed_data.identity_match_scores
-    # name: str
-    # description: Optional[str] = None
-    # price: float
-    # tax: Optional[float] = None
+
 
 class ScoresFrameUI(ScoresFrameAPI):
-
-    def prepare_data_for_ui(self) -> pd.Dataframe:
+    data_for_ui: dict
+    def prepare_data_for_ui(self) -> dict:
         """Prepare an extended and refined version of the 'clean_data' frame"""
         self.data.clean_data["score"] = self.data.identity_match_scores
         for col in ["birthdate1", "birthdate2"]:
             self.data.clean_data[col] = self.data.clean_data[col].dt.strftime("%Y-%m-%d")
-        return self.data.clean_data.to_dict(orient="index")
+        self.data_for_ui = self.data.clean_data.to_dict(orient="index")
 
 
 def check_api_key(api_key: str) -> str:
@@ -67,10 +66,8 @@ def get_current_username(credentials: HTTPBasicCredentials = Depends(security)):
 @app.get("/service_api_default")
 async def export_default_results(apikey: str):
     if check_api_key(apikey):
-        # results = prepare_results(default_data_path)
         results = ScoresFrameAPI()
         results.prepare_results()
-        # return {"identity_match_scores": results.identity_match_scores}
         return results
 
 
@@ -80,22 +77,15 @@ async def export_results(filepath: str, apikey: str):
         results = ScoresFrameAPI()
         results.prepare_results(filepath)
         return results
-        # results = prepare_results(filepath)
-        # return {"identity_match_scores": results.identity_match_scores}
 
 
 @app.get("/service_ui", response_class=HTMLResponse)
 def display_results(request: Request, username: str = Depends(get_current_username)):
 
-    # data = prepare_results()
     results = ScoresFrameUI()
     results.prepare_results()
-    # data.clean_data["score"] = data.identity_match_scores
-    # for col in ["birthdate1", "birthdate2"]:
-    #     data.clean_data[col] = data.clean_data[col].dt.strftime("%Y-%m-%d")
-    # output = data.clean_data.to_dict(orient="index")
-    data = results.prepare_data_for_ui()
+    results.prepare_data_for_ui()
 
     return templates.TemplateResponse(
-        "display_results.html", {"request": request, "data": data}
+        "display_results.html", {"request": request, "data": results.data_for_ui}
     )
